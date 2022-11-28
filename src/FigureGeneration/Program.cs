29810dd90a163542609e714f63bc7ee9f79a61c2
James Hughes <james@pyrosoftsolutions.co.uk>
2022-11-28 01:24:49 +0000

    Updated gitignore and added missed file

---
+++ src/FigureGeneration/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.Json;
+using DiscosWebSdk.Models.ResponseModels.DiscosObjects;
+using FigureGeneration;
+using FigureGeneration.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+ServiceProvider serviceProvider = Bootstrap.Up();
+PlotGeneratorService pgService = serviceProvider.GetRequiredService<PlotGeneratorService>();
+await pgService.GenerateAllPlots();
+
+
+const string dataRoot      = "/home/james/repos/DiscosWebAnalysis/data/";
+string discosObjectsJson = File.ReadAllText($"{dataRoot}20221125/discos-DiscosObject-20221125.json");
+
+List<DiscosObject>? discosObjects = JsonSerializer.Deserialize<List<DiscosObject>>(discosObjectsJson);
+
